19f02d9a10c2683bbfb95f057db7c7ae339eff07
gnikoloudakis <hose.cuervo@gmail.com>
2017-03-27 20:03:14 +0300

    oo

--- tools/testSerial.py
+++ tools/testSerial.py
@@ -1,11 +1,15 @@
 import time
 import serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0',
+                    baudrate=9600,
+                    timeout=1
+                    )
 
-# while 1:
-ser.write('v\r\n'.encode())
-time.sleep(1)
-print(ser.readline())
-ser.close()
+while 1:
+    ser.write('v\r\n'.encode())
+    time.sleep(2)
+    x = ser.readline()
+    print x
+
 
