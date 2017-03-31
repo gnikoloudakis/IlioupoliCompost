7f24bd5148ed8cd94e4e01071d8c6d83765fb9df
gnikoloudakis <hose.cuervo@gmail.com>
2017-03-31 17:03:27 +0300

    pasiphae

--- modules/SetupSchedulers.py
+++ modules/SetupSchedulers.py
@@ -64,7 +64,7 @@ def systemrestart():
 def init_sched():
     scheduler.add_job(pushSoilBack, 'cron', day_of_week='mon-fri', hour=datetime.datetime.strptime(Settings.objects.first().daily_soil_backward_time, '%H:%M%p').hour, jobstore='default', executor='default', replace_existing=True, id='soilBack')
     scheduler.add_job(soilHomogenization, 'cron', day_of_week='mon-fri', hour=datetime.datetime.strptime(Settings.objects.first().daily_steering_time, '%H:%M%p').hour, jobstore='default', executor='default', replace_existing=True, id='soilHomogenization')
-    scheduler.add_job(systemrestart, 'cron', day_of_week='mon-fri', hour=24, jobstore='default', executor='default', replace_existing=True, id='sysRestart')
+    # scheduler.add_job(systemrestart, 'cron', day_of_week='mon-fri', hour=24, jobstore='default', executor='default', replace_existing=True, id='sysRestart')
 
     # scheduler2.add_job(add_measurements, 'interval', minutes=1)#, jobstore='default', executor='default', replace_existing=True, id='add_meas')
     scheduler2.add_job(hourlyVentilation, 'interval', hours=1)  # , jobstore='default', executor='default', replace_existing=True, id='vent')
